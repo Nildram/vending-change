6d2ab6eba863ef806e7f1eba2fd304421061238d
Jeremy Pollard <email.pollard@gmail.com>
2020-05-09 19:31:48 +0100

    Refactor tests

--- test/test_change_algorithm.py
+++ test/test_change_algorithm.py
@@ -34,26 +34,23 @@ class TestAlgorithms(unittest.TestCase):
 
     def _run_canonical_coins(self, algorithm: ChangeAlgorithm):
         test_cases = [
-            ({}, 1),
-            ({2:1}, 1),
-            ({1:1}, 2)
-        ]
-        for test_case in test_cases:
-            with self.subTest(f"coins: {test_case[0]}, amount: {test_case[1]}"):
-                self.assertRaises(CalculationError, algorithm.calculate_coins, algorithm.sort_coins(test_case[0]), test_case[1])
-
-        test_cases = [
             # expected, coins, amount
-            # ({}, {1:1}, 0),
-            # ({1:1}, {1:1}, 1),
-            # ({1:10}, {1:10}, 10),
-            # ({2:5}, {2:5}, 10),
-            # ({2:3, 1:4}, {2:3, 1:10}, 10),
+            ({}, {1:1}, 0),
+            ({1:1}, {1:1}, 1),
+            ({1:10}, {1:10}, 10),
+            ({2:5}, {2:5}, 10),
+            ({2:3, 1:4}, {2:3, 1:10}, 10),
             ({200: 1, 100: 1, 50: 1, 20: 1, 10: 1, 5: 1, 2: 1, 1:1}, {200:10, 100:10, 50:10, 20:10, 10:10, 5:10, 2:10, 1:10}, 388)
         ]
         for test_case in test_cases:
             with self.subTest(f"coins: {test_case[1]}, amount: {test_case[2]}"):
-                self.assertEqual(test_case[0], algorithm.calculate_coins(test_case[1], test_case[2]))
+                self.assertEqual(test_case[0], algorithm.calculate_coins(algorithm.sort_coins(test_case[1]), test_case[2]))
 
-        self.assertEqual({200: 1, 100: 1, 50: 1, 20: 1, 10: 1, 5: 1, 2: 1, 1:1},
-            algorithm.calculate_coins(algorithm.sort_coins({200:10, 100:10, 50:10, 20:10, 10:10, 5:10, 2:10, 1:10}), 388))
+        exception_test_cases = [
+            ({}, 1),
+            ({2:1}, 1),
+            ({1:1}, 2)
+        ]
+        for test_case in exception_test_cases:
+            with self.subTest(f"coins: {test_case[0]}, amount: {test_case[1]}"):
+                self.assertRaises(CalculationError, algorithm.calculate_coins, algorithm.sort_coins(test_case[0]), test_case[1])
